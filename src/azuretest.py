53a475668823eb9021f61364597deb3c4799a112
bahorn <horn@uni.coventry.ac.uk>
2017-11-25 14:25:44 +0000

    Added full support for Analyze Image

--- src/azuretest.py
+++ src/azuretest.py
@@ -9,14 +9,19 @@ class azureComputerVision:
         self.apikey = apikey
         self.endpoint = "https://{}.api.cognitive.microsoft.com/vision/v1.0".format(zone)
     # pass a URL/file to the computer vision api.
-    def queryComputerVision(self, input):
+    def queryComputerVision(self, input, visualFeatures=None, details=None):
         headers = {'Ocp-Apim-Subscription-Key': self.apikey}
+        query = []
+        if visualFeatures != None:
+            query += ["visualFeatures="+",".join(visualFeatures)]
+        if details != None:
+            query += ["details="+",".join(details)]
         if type(input) == str:
             data = {'url':input}
-            r = requests.post(self.endpoint+"/analyze?visualFeatures=tags",
+            r = requests.post(self.endpoint+"/analyze?{}".format("&".join(query)),
                 headers=headers, json=data)
         elif type(input) == file:
-            r = requests.post(self.endpoint+"/analyze?visualFeatures=tags",
+            r = requests.post(self.endpoint+"/analyze?{}".format("&".join(query)),
                 headers=headers, files={'file': input})
         else:
             raise TypeError
@@ -30,4 +35,4 @@ if __name__ == "__main__":
     load_dotenv(dotenv_path)
     apikey = os.environ['KEY1']
     cv = azureComputerVision(apikey)
-    print cv.queryComputerVision("https://avatars2.githubusercontent.com/u/22912854?s=400&u=ee48be5a1fcb4aca344cbad45509408022073b64&v=4")
+    print cv.queryComputerVision(file(sys.argv[1]), visualFeatures=['faces'])
