046450b8d129d00fc38af44da0361bff2401e7b6
bahorn <horn@uni.coventry.ac.uk>
2017-11-25 14:13:36 +0000

    made a library, it supports files/urls

--- src/azuretest.py
+++ src/azuretest.py
@@ -1,23 +1,33 @@
 import os
+import sys
 import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-def queryComputerVision(url, apikey=None):
-    endpoint = "https://northeurope.api.cognitive.microsoft.com/vision/v1.0"
-    headers = {'Ocp-Apim-Subscription-Key': apikey}
-    data = {'url':url}
-    r = requests.post(endpoint+"/analyze?visualFeatures=tags",
-            headers=headers, json=data)
-    if r.status_code == 200:
-        return r.json()
-    else:
-        return None
+class azureComputerVision:
+    def __init__(self, apikey, zone="northeurope"):
+        self.apikey = apikey
+        self.endpoint = "https://{}.api.cognitive.microsoft.com/vision/v1.0".format(zone)
+    # pass a URL/file to the computer vision api.
+    def queryComputerVision(self, input):
+        headers = {'Ocp-Apim-Subscription-Key': self.apikey}
+        if type(input) == str:
+            data = {'url':input}
+            r = requests.post(self.endpoint+"/analyze?visualFeatures=tags",
+                headers=headers, json=data)
+        elif type(input) == file:
+            r = requests.post(self.endpoint+"/analyze?visualFeatures=tags",
+                headers=headers, files={'file': input})
+        else:
+            raise TypeError
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return None
 
 if __name__ == "__main__":
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     apikey = os.environ['KEY1']
-    #url = 'https://upload.wikimedia.org/wikipedia/commons/1/1d/White_House_north_and_south_sides.jpg'
-    url = 'https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg'
-    print queryComputerVision(url, apikey=apikey)
+    cv = azureComputerVision(apikey)
+    print cv.queryComputerVision("https://avatars2.githubusercontent.com/u/22912854?s=400&u=ee48be5a1fcb4aca344cbad45509408022073b64&v=4")
