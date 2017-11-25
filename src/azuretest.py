5e4e310e6b3bd48d748c228e081a0a599990fae0
bahorn <horn@uni.coventry.ac.uk>
2017-11-25 16:40:56 +0000

    Now supports the face API

--- src/azuretest.py
+++ src/azuretest.py
@@ -7,19 +7,22 @@ from dotenv import load_dotenv
 class azureComputerVision:
     def __init__(self, apikey, zone="northeurope"):
         self.apikey = apikey
-        self.endpoint = "https://{}.api.cognitive.microsoft.com/vision/v1.0".format(zone)
+        self.zone = zone
     
-    def _queryComputerVision(self, method, input, query):
+    def _queryComputerVision(self, method, input, query, api='vision'):
         headers = {'Ocp-Apim-Subscription-Key': self.apikey}
+        endpoint = "https://{}.api.cognitive.microsoft.com/{}/v1.0".format(
+                self.zone, api)
         if type(input) == str:
             data = {'url':input}
-            r = requests.post(self.endpoint+"/{}?{}".format(method, "&".join(query)),
+            r = requests.post(endpoint+"/{}?{}".format(method, "&".join(query)),
                 headers=headers, json=data)
         elif type(input) == file:
-            r = requests.post(self.endpoint+"/{}?{}".format(method, "&".join(query)),
+            r = requests.post(endpoint+"/{}?{}".format(method, "&".join(query)),
                 headers=headers, files={'file': input})
         else:
             raise TypeError
+        print r.text
         if r.status_code == 200:
             return r.json()
         else:
@@ -39,10 +42,19 @@ class azureComputerVision:
         query += ['detectOrientation={}'.format(str(detectOrientation))]
         return self._queryComputerVision('ocr', input, query)
 
+    def detectFace(self, input, returnFaceId=True, returnFaceLandmarks=False,
+            returnFaceAttributes=None):
+        query = []
+        query += ['returnFaceId={}'.format(returnFaceId)]
+        query += ['returnFaceLandmarks={}'.format(returnFaceLandmarks)]
+        if returnFaceAttributes != None:
+            query += ["returnFaceAttributes="+','.join(returnFaceAttributes)]
+        return self._queryComputerVision('detect', input, query, api='face')
+
 if __name__ == "__main__":
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
-    apikey = os.environ['KEY1']
+    apikey = os.environ['FACEKEY']
     cv = azureComputerVision(apikey)
-    print cv.queryOCR(file(sys.argv[1]))
-    #print cv.queryComputerVision(file(sys.argv[1]), visualFeatures=['tags'])
+    #print cv.queryOCR(file(sys.argv[1]))
+    print cv.detectFace('https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzg5ODI4MTEw/barack-obama-12782369-1-402.jpg')
