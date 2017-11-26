58b151b8ed8a51b2a675bc0902d0aec062f387b5
bahorn <horn@uni.coventry.ac.uk>
2017-11-26 02:28:40 +0000

    added flask-CORS

--- src/medical/backend.py
+++ src/medical/backend.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import json
 import os
 from pymongo import MongoClient
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/search")
 def search():
