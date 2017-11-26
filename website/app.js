5ab9866f8b260d05eb057859ff675838f574002b
James Vickery <dev@jamesvickery.net>
2017-11-26 00:56:38 +0000

    Redesigned, started AngularJS app

---
+++ website/app.js
@@ -0,0 +1,9 @@
+angular.module('patientPapers', [])
+    .controller('PatientPapersController', function () {
+        var patientPapers = this
+
+        this.patient = {
+            name: 'Name Surname'
+        }
+        
+    });
