e5855f5177bdd7918ed70edcfaddadcc6d6b6e16
bahorn <horn@uni.coventry.ac.uk>
2017-11-26 06:09:21 +0000

    it now sets up the conditions in the deploy.sh script.

--- src/medical/deploy.sh
+++ src/medical/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+python condition_data.py
 python install_to_mongo.py
 printf "use test_database\ndb.$COLLECTION.createIndex({name:\"text\",surname:\"text\",gender:\"text\",birthdate:\"text\"})" | mongo
