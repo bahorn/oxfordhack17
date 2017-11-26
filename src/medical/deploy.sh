4cc4406c3acba1c9f51d87564bcc1967ec2d6ed1
bahorn <horn@uni.coventry.ac.uk>
2017-11-26 02:22:52 +0000

    now it really deploys

--- src/medical/deploy.sh
+++ src/medical/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python install_to_mongo.py
-echo "db.$COLLECTION.createIndex({name:"text",surname:"text",gender:"text",birthdate:"text"})" | mongo
+printf "use test_database\ndb.$COLLECTION.createIndex({name:\"text\",surname:\"text\",gender:\"text\",birthdate:\"text\"})" | mongo
